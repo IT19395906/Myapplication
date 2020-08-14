07a4e2a49ceb1f98cfc500e069980415b440e272
IT19395906 <IT19395906@my.sliit.lk>
2020-08-15 01:45:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mad/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView txt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button1);
+        txt = findViewById(R.id.textview1);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MainActivity.this,Second.class);
+                startActivity(intent);
+                //txt.setText("thank you for coming");
+            }
+        });
+    }
+}
